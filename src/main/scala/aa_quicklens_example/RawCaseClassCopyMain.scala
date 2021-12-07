1e87ed382d5bef757e3c3de1c35ea0455d759bcc
mveeprojects <markvallis1987@Gmail.com>
2021-12-07 20:03:03 +0000

    Added simple QuickLense vs raw copy example

--- src/main/scala/aa_quicklens_example/RawCaseClassCopyMain.scala
+++ src/main/scala/aa_quicklens_example/RawCaseClassCopyMain.scala
@@ -6,14 +6,13 @@ object RawCaseClassCopyMain extends App {
 
   println(s"original user:\n$user\n")
 
-  val newBillingInfo = user.billingInfo.copy(creditCards = List.empty)
+  val newBillingInfo              = user.billingInfo.copy(creditCards = List.empty)
   val userCopiedWithNoCreditCards = user.copy(billingInfo = newBillingInfo)
 
   println(s"userCopiedWithNoCreditCards:\n$userCopiedWithNoCreditCards\n")
 
-  val newName = user.name.copy(lastName = "Vee")
-  val newPhone = "6789"
-  val userCopiedAndChangedTwoFields = user.copy(name = newName, phone = newPhone)
+  val newName                       = user.name.copy(lastName = "Vee")
+  val userCopiedAndChangedTwoFields = user.copy(name = newName, phone = "6789")
 
   println(s"userCopiedAndChangedTwoFields:\n$userCopiedAndChangedTwoFields \n")
 }
